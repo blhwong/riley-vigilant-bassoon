85f479cf9f5e837646cfeab5d8cdf5c23a489b35
Brandon Wong <b.lh.wong@gmail.com>
2017-06-02 20:27:27 -0700

    Add passport functions

--- server/index.js
+++ server/index.js
@@ -30,6 +30,30 @@ app.use(passport.session());
 
 app.use(express.static(path.resolve(__dirname, '..', 'public', 'dist')));
 
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: 'http://localhost:3000/auth/google/callback',
+  scope: ['openid', 'email', 'https://mail.google.com/', 'https://www.googleapis.com/auth/gmail.modify', 'https://www.googleapis.com/auth/gmail.readonly']
+},
+  function(accessToken, refreshToken, profile, done) {
+    process.nextTick(function() {
+      return done(null, profile);
+    });
+  }
+));
+
+app.get('/login',
+passport.authenticate('google', { scope: ['openid', 'email', 'https://mail.google.com/', 'https://www.googleapis.com/auth/gmail.modify', 'https://www.googleapis.com/auth/gmail.readonly']
+}));
+
+app.get( '/auth/google/callback',
+passport.authenticate( 'google', {
+  failureRedirect: '/login'
+}), (req, res) => {
+  res.redirect('/');
+});
+
 app.get('*', (req, res) => {
   res.sendFile( path.resolve(__dirname, '..', 'public', 'dist', 'index.html'));
 });
