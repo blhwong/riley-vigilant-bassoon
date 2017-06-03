8afb2197ef3c50bc3c5d991d28de04d65497183a
Brandon Wong <b.lh.wong@gmail.com>
2017-06-02 20:56:45 -0700

    Add componentDidMount to get threads and state

--- public/src/index.jsx
+++ public/src/index.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+import Threads from './components/Threads.jsx';
+import Login from './components/Login.jsx';
+import io from 'socket.io-client';
+import 'bootstrap/less/bootstrap.less';
 
 class App extends React.Component {
   constructor(props) {
@@ -8,6 +13,39 @@ class App extends React.Component {
       threads: [],
       authenticated: false
     };
+  }
+
+  componentDidMount() {
+    console.log(this.state.authenticated);
+    axios.get('/auth')
+    .then((results) => {
+      this.setState({
+        authenticated: results.data
+      });
+      if (this.state.authenticated) {
+        console.log('getting messages');
+        return axios.get('/messages');
+      }
+    })
+    .then((results) => {
+      console.log('success!', results);
+      if (!!results) {
+        this.setState({
+          threads: results.data
+        });
+        let socket = io.connect('http://localhost:3000');
+        socket.on('new-mail', (data) => {
+          console.log('receive', data);
+          this.setState({
+            threads: data
+          });
+        });
+      }
+      console.log(this.state.threads);
+    })
+    .catch((err) => {
+      console.error('err', err);
+    });
   }
 
   render () {
