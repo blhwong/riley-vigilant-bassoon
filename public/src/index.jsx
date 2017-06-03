5a4a09bb3549518a810fd97c70914c2b204b8e59
Brandon Wong <b.lh.wong@gmail.com>
2017-06-02 21:13:21 -0700

    Cleanup code

--- public/src/index.jsx
+++ public/src/index.jsx
@@ -16,32 +16,27 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    console.log(this.state.authenticated);
     axios.get('/auth')
     .then((results) => {
       this.setState({
         authenticated: results.data
       });
       if (this.state.authenticated) {
-        console.log('getting messages');
         return axios.get('/messages');
       }
     })
     .then((results) => {
-      console.log('success!', results);
       if (!!results) {
         this.setState({
           threads: results.data
         });
         let socket = io.connect('http://localhost:3000');
         socket.on('new-mail', (data) => {
-          console.log('receive', data);
           this.setState({
             threads: data
           });
         });
       }
-      console.log(this.state.threads);
     })
     .catch((err) => {
       console.error('err', err);
